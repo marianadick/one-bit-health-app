e98a294aaba7174ee4f0ef1b0088893d19ca075d
marianadick <dickmariana1@gmail.com>
2021-11-25 00:34:04 -0300

    criaçao do app

---
+++ components/Form/ResultIMC/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { View, Text } from "react-native"
+import styles from "./style";
+
+export default function ResultIMC(props){
+    return(
+        <View style={styles.resultImc}>
+            <Text style={styles.information}>{props.messageResultIMC}</Text>
+            <Text style={styles.numberImc}>{props.resultIMC}</Text>
+        </View>
+    );
+}
